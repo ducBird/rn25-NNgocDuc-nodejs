738087c4d2d3f4ed17d6bf9a28efa2f896b3af44
DucNguyen <nguyenngocduc94751@gmail.com>
2022-11-03 15:48:50 +0700

    commit 3k file code

---
+++ node-practices/MongoDB/Categories/create.js
@@ -0,0 +1,18 @@
+const { default: mongoose } = require('mongoose');
+const { Category } = require('../models');
+// MONGOOSE
+mongoose.connect('mongodb://localhost:27017/BasicDBecommerce');
+
+try {
+  const data = {
+    name: 'Thời trang',
+    description: 'Mô tả ...',
+  };
+
+  const newItem = new Category(data);
+  newItem.save().then((result) => {
+    console.log(result);
+  });
+} catch (err) {
+  console.log(err);
+}
